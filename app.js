063776c9ffef7110539505ffd92420a3c5bc8533
sriramadari <lakshmisriramadari1427@gmail.com>
2023-12-25 00:30:05 +0530

    implemented socketio

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+// index.js
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const port = 3000;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_AI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-const run=async()=>{
-    // For text-only input, use the gemini-pro model  
-    try{
-    const prompt = "What special about google gemini AI?"
-  
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-    }catch(err){
-        console.log(err)
-    }
-  }
-  run();
+const corsOptions = {
+  origin: "*", // Allow all origins
+  credentials: true // Enable credentials
+};
+app.use(cors(corsOptions));
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: corsOptions // Apply the same CORS options to Socket.IO
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('generate', (prompt) => {
+    console.log('Generating content for prompt:', prompt);
+    model
+      .generateContentStream(prompt)
+      .then(async (result) => {
+        for await (const chunk of result.stream) {
+          const chunkText = chunk.text();
+          console.log(chunkText);
+
+          // Send chunkText to the client
+          socket.emit('content', chunkText);
+        }
+
+        // Indicate the end of the stream
+        socket.emit('content', 'End of Content');
+      })
+      .catch((err) => {
+        console.error('Error generating content:', err.message);
+      });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running 🚀⚡`);
+});
