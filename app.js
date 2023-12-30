76c286be8a56880bceeb6bcc5addc93e9baa87cd
sriramadari <lakshmisriramadari1427@gmail.com>
2023-12-30 20:50:05 +0530

    gemini-pro-vision model for image processing

--- app.js
+++ app.js
@@ -10,47 +10,80 @@ const app = express();
 const port = 3000;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_AI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-
+// const generationConfig = {
+//   stopSequences: ["red"],
+//   maxOutputTokens: 2000,
+//   temperature: 0.9,
+//   topP: 0.1,
+//   topK: 16,
+// };
+// For text-and-image input (multimodal)
 const corsOptions = {
   origin: "*", // Allow all origins
   credentials: true // Enable credentials
 };
 app.use(cors(corsOptions));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: corsOptions // Apply the same CORS options to Socket.IO
 });
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
+let socket;
 
-  socket.on('generate', (prompt) => {
-    console.log('Generating content for prompt:', prompt);
-    model
-      .generateContentStream(prompt)
-      .then(async (result) => {
-        for await (const chunk of result.stream) {
-          const chunkText = chunk.text();
-          console.log(chunkText);
-
-          // Send chunkText to the client
-          socket.emit('content', chunkText);
-        }
-
-        // Indicate the end of the stream
-        socket.emit('content', 'End of Content');
-      })
-      .catch((err) => {
-        console.error('Error generating content:', err.message);
-      });
-  });
-
+io.on('connection', (socketConnection) => {
+  console.log('client connected');
+  socket=socketConnection;
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
+
+app.post('/generate', async (req, res) => {
+  const { base64Image, prompt } = req.body
+  // console.log(base64Image, prompt);
+  const imagePart = {
+    inlineData: {
+      data: base64Image,
+      mimeType: 'image/png'
+    },
+  };
+  try {
+    let result
+    if(base64Image){
+      const model = genAI.getGenerativeModel({ model: "gemini-pro-vision"});
+      result = await model.generateContentStream([prompt, imagePart]);
+    } else {
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      result = await model.generateContentStream(prompt);
+    }
+    
+    let text = '';
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      console.log(chunkText);
+      text += chunkText;
+      if (socket) {
+        try {
+          socket.emit('content', chunkText);
+        } catch (error) {
+          console.error('Error emitting content:', error.message);
+        }
+      }
+    }
+    if (socket) {
+      socket.emit('content', "hope this is useful :)");
+      socket.disconnect();
+    }
+    res.status(200).json({message:"success"});
+  } catch (err) {
+    console.error('Error generating content:', err.message);
+    res.status(500).json({ error: 'Error generating content' });
+  }
+});
+
 
 server.listen(port, () => {
   console.log(`Server is running 🚀⚡`);
